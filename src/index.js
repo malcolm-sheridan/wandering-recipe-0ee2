20405f7768166fcf3f1cd8ecac705e6def2cd863
Malcolm Sheridan <malcolm.sheridan@kolmeo.com>
2025-07-25 15:01:36 +1000

    Remove commented-out 404 response handling in fetch function

--- src/index.js
+++ src/index.js
@@ -53,13 +53,13 @@ export default {
 		yogaVideoManager.getYogaVideosByAgeRange(env, ageRange);
 	}
 
-	if(!data.message) {
-		// If no message is set, return a 404
-		return new Response(JSON.stringify({error: "Not found"}), {
-			status: 404,
-			headers: { "Content-Type": "application/json" }
-		});
-	}
+	// if(!data.message) {
+	// 	// If no message is set, return a 404
+	// 	return new Response(JSON.stringify({error: "Not found"}), {
+	// 		status: 404,
+	// 		headers: { "Content-Type": "application/json" }
+	// 	});
+	// }
 
 	
   },
